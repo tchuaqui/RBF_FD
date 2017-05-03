127c01284f918c29bee100c8bedc01a02f61818a
tchuaqui <tomaschuaqui@gmail.com>
2017-05-03 14:44:15 +0100

    testes

--- timoshenko_local.m
+++ timoshenko_local.m
@@ -2,7 +2,7 @@ clear all
 x_inicial=0;x_final=100e-3;
 L=x_final-x_inicial;
 cfstr='cl';
-n=100;
+n=150;
 x_dados=[0:L/(n-1):L];dist=x_dados(3)-x_dados(1);
 [xi,xj]=meshgrid(x_dados);
 x_central=find(x_dados==0.5);
